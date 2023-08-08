ab2718294764562484339b880c1c6d67f6a117fb
Tiago Baratieri <tiagobaratieri97@gmail.com>
2023-08-08 11:16:52 -0300

    GET Product paged

--- backend/src/main/java/com/baratieri/dscommerce/services/ProductService.java
+++ backend/src/main/java/com/baratieri/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.baratieri.dscommerce.dto.ProductDTO;
 import com.baratieri.dscommerce.entities.Product;
 import com.baratieri.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,8 +20,14 @@ public class ProductService {
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
         Optional<Product> result = repository.findById(id);
-        Product product = result.get();
+        Product product = result.orElseThrow(() -> new RuntimeException("Product not found")); // Tratamento para produto não encontrado
         return new ProductDTO(product);
-
+    }
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> finAll(Pageable pageable) {
+        Page<Product> list = repository.findAll(pageable);
+        return list.map(x -> new ProductDTO(x));
     }
 }
+
+
